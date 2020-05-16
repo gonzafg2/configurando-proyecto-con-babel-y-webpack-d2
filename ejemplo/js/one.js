0e7dca8676bddb6895c8ea5fdcb7cb61569a1b3c
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-16 19:37:31 -0400

    Cambio función a nueva sintaxis con ES6

--- ejemplo/js/one.js
+++ ejemplo/js/one.js
@@ -1,17 +1,26 @@
 let oneArray = [1, 4, 23, -4, "one", 6, 0, 1.1, 3.1415];
 
-function multiplyAndFilter(entry) {
+// function multiplyAndFilter(entry) {
   
-  let multiply = [];
+//   let multiply = [];
 
+//   for (let i in entry) {
+//     multiply[i] = entry[i] * 4;
+//   }
+
+//   let result = multiply.filter(filtro => filtro >= 8);
+
+//   return result;
+
+// }
+
+const multiplyAndFilter = (entry) => {
+  let multiply = [];
   for (let i in entry) {
     multiply[i] = entry[i] * 4;
   }
-
-  let result = multiply.filter(filtro => filtro >= 8);
-
+  let result = multiply.filter((filtro) => filtro >= 8);
   return result;
-
 }
 
 console.log(multiplyAndFilter(oneArray));
